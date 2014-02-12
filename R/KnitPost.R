4bcd9421101271389bfcc2dd7d92b7ae21415b1c
bschneeman <bschneeman@homeaway.com>
2014-02-12 11:31:54 -0600

    Making it schnee

--- R/KnitPost.R
+++ R/KnitPost.R
@@ -4,12 +4,12 @@
 #' Also includes ideas from https://github.com/supstat/vistat/blob/gh-pages/_bin/knit
 #' @export
 #' @examples
-#' \dontrun{library(devtools); install_github('knitPost', 'cpsievert')}
+#' \dontrun{library(devtools); install_github('knitPost', 'schnee')}
 #' library(knitPost)
 #' setwd("~/Desktop/github/local/cpsievert.github.com/")
 #' knitPost("2013-05-15-hello-jekyll.Rmd")
 
-knitPost <- function(post="2013-05-15-hello-jekyll.Rmd", baseUrl="http://cpsievert.github.com/") {
+knitPost <- function(post="2013-05-15-hello-jekyll.Rmd", baseUrl="http://schnee.github.com/") {
   sourcePath <- file.path(getwd(), "_source", post)
   opts_knit$set(base.url=baseUrl)
   base <- sub("\\.[Rr]md$", "", basename(sourcePath))
